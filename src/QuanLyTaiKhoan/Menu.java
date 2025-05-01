88b0b23e62ad70dce8568edd4ddaeaffad877ba9
Nguyen Tuan <nguyenquoctuan020803@gmail.com>
2025-05-01 10:14:24 +0700

    cap nhat giao dien

--- src/QuanLyTaiKhoan/Menu.java
+++ src/QuanLyTaiKhoan/Menu.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
-
-* and open the template in the editor.
- */
 package QuanLyTaiKhoan;
 
 
 import java.util.Scanner;
 import java.util.Vector;
 
-
-/**
- *
- */
 public abstract class Menu {
 
 
